a5895ca934aa7c38644904b9fe6c1f9afe7d1a2a
UnknxxwnDEV <36553113+Unknxxwn@users.noreply.github.com>
2025-01-09 21:56:02 +0100

    Aufgabe 10.2 a

---
+++ src/org/hbrs/se1/ws24/exercises/uebung10/MyStack.java
@@ -0,0 +1,46 @@
+package org.hbrs.se1.ws24.exercises.uebung10;
+
+import java.util.EmptyStackException;
+import java.util.Stack;
+
+public class MyStack<E> extends Stack<E> {
+    private final int maxSize;
+
+    public MyStack(int maxSize) {
+        super();
+        this.maxSize = maxSize;
+
+    }
+
+    @Override
+    public E push(E item) {
+        if (size() >= maxSize) {
+            throw new IllegalStateException();
+        }
+        return super.push(item);
+    }
+
+    @Override
+    public synchronized E pop() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return super.pop();
+    }
+
+    @Override
+    public synchronized E peek() {
+        return super.peek();
+    }
+
+    @Override
+    public synchronized int size() {
+        return super.size();
+    }
+
+    @Override
+    public synchronized boolean isEmpty() {
+        return super.isEmpty();
+    }
+
+}
---
+++ test/test/org/hbrs/se1/ws24/tests/Uebung10/MyStackTest.java
@@ -0,0 +1,38 @@
+package test.org.hbrs.se1.ws24.tests.Uebung10;
+
+import org.hbrs.se1.ws24.exercises.uebung10.MyStack;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class MyStackTest {
+    private MyStack<Integer> stack;
+
+    @BeforeEach
+    public void setUp() {
+    }
+
+    @Test
+    void testIsEmpty() {
+
+    }
+
+    @Test
+    void testPeek() {
+
+    }
+
+    @Test
+    void testPop() {
+
+    }
+
+    @Test
+    void testPush() {
+
+    }
+
+    @Test
+    void testSize() {
+
+    }
+}
